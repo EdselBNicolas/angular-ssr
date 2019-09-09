770be0f2759d77df6e799f1d0d548833830f61ce
EdselBNicolas <ebnicolas.halcyondigital@gmail.com>
2019-09-09 09:15:11 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
+import { Blob } from 'blob-polyfill';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private titleService: Title, private metaService: Meta) { }
   title = 'angular-ssr';
+  ngOnInit(): void {
+    this.titleService.setTitle('Cheers!');
+  }
 }
